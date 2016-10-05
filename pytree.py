c2fbc6a8445834970497ed1e32897606082ed138
Mengjiao ZHU <mengjiao0326@gmail.com>
2016-10-05 12:05:12 -0400

    Update pytree.py

--- pytree.py
+++ pytree.py
@@ -17,10 +17,6 @@ end_branch = '└── '
 def print_tree(cpath, padding):
     children = [child for child in os.listdir(cpath) if not child.startswith('.')]
     children = sorted(children, key=lambda x: re.sub('[^a-zA-Z0-9]+', '', x).lower())
-    chidren_in_array = []
-    for child in children:
-        if child[0] != '.':
-            chidren_in_array.append(child)
     dir_num = 0
     file_num = 0
     for index in range(len(children)):
@@ -53,8 +49,5 @@ if __name__ == '__main__':
         else:
             current_path = temp
         print(temp)
-    # else:
-    #     print("Too many parameters! Please check it!")
-    #     sys.exit()
     dir_num, file_num = print_tree(current_path, "")
     print('\n' + str(dir_num) + " directories, " + str(file_num) + " files")
